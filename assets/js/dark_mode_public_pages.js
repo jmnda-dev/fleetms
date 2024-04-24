a5785d4427c7f85a261b77c37e36e25359389998
Jachin Manda <manda.jachin482@gmail.com>
2024-04-25 00:39:10 +0200

    add separate js files for public pages

---
+++ assets/js/dark_mode_public_pages.js
@@ -0,0 +1,50 @@
+const themeToggleDarkIcon = document.getElementById('themeToggleDarkIcon');
+const themeToggleLightIcon = document.getElementById('themeToggleLightIcon');
+
+if (themeToggleDarkIcon && themeToggleLightIcon) {
+    // Change the icons inside the button based on previous settings
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        themeToggleLightIcon.classList.remove('hidden');
+    } else {
+        themeToggleDarkIcon.classList.remove('hidden');
+    }
+}
+
+const themeToggleBtn = document.getElementById('themeToggle');
+
+let event = new Event('dark-mode');
+
+if (themeToggleBtn) {
+
+    themeToggleBtn.addEventListener('click', function() {
+
+        // toggle icons
+        themeToggleDarkIcon.classList.toggle('hidden');
+        themeToggleLightIcon.classList.toggle('hidden');
+
+        // if set via local storage previously
+        if (localStorage.getItem('color-theme')) {
+            if (localStorage.getItem('color-theme') === 'light') {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            }
+
+        // if NOT set via local storage previously
+        } else {
+            if (document.documentElement.classList.contains('dark')) {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            } else {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            }
+        }
+
+        document.dispatchEvent(event);
+        
+    });
+
+}
