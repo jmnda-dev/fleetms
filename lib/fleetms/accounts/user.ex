ddd7e21aeb50410ed9236273ded453b9cfd0160d
Jachin Manda <manda.jachin482@gmail.com>
2024-04-29 02:03:18 +0200

    use regex to validate email and password

--- lib/fleetms/accounts/user.ex
+++ lib/fleetms/accounts/user.ex
@@ -70,17 +70,66 @@ defmodule Fleetms.Accounts.User do
       allow_nil_input [:hashed_password]
       accept [:email]
 
-      argument :password, :string, sensitive?: true
-      argument :password_confirmation, :string, sensitive?: true
+      argument :password, :string do
+        sensitive? true
+        constraints min_length: 8, max_length: 32
+      end
+
+      argument :password_confirmation, :string do
+        sensitive? true
+        constraints min_length: 8, max_length: 32
+      end
+
       argument :user_profile, :map, allow_nil?: false
       argument :organization, :map, allow_nil?: false
 
-      validate AshAuthentication.Strategy.Password.PasswordConfirmationValidation
+      validate match(:email, ~r/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/) do
+        message "Invalid email format"
+      end
 
+      validate match(:password, ~r/^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/) do
+        message "Invalid password format. Password much contain at least one lowercase and uppercase letter, and at leat one of these characters: !#$%&?"
+      end
+
+      validate AshAuthentication.Strategy.Password.PasswordConfirmationValidation
       change AshAuthentication.Strategy.Password.HashPasswordChange
       change AshAuthentication.GenerateTokenChange
 
       change manage_relationship(:organization, type: :direct_control)
+      change manage_relationship(:user_profile, type: :direct_control)
+    end
+
+    create :organization_internal_user do
+      allow_nil_input [:hashed_password]
+      accept [:email]
+
+      argument :password, :string do
+        sensitive? true
+        constraints min_length: 8, max_length: 32
+      end
+
+      argument :password_confirmation, :string do
+        sensitive? true
+        constraints min_length: 8, max_length: 32
+      end
+
+      argument :user_profile, :map, allow_nil?: false
+      argument :organization_id, :uuid, allow_nil?: false
+
+      validate match(:email, ~r/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/) do
+        message "Invalid email format"
+      end
+
+      validate match(:password, ~r/^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/) do
+        message "Invalid password format. Password much contain at least one lowercase and uppercase letter, and at leat one of these characters: !#$%&?"
+      end
+
+      validate AshAuthentication.Strategy.Password.PasswordConfirmationValidation
+      change set_context(%{strategy_name: :password})
+      change AshAuthentication.Strategy.Password.HashPasswordChange
+      change AshAuthentication.GenerateTokenChange
+
+      change manage_relationship(:organization_id, :organization, type: :append_and_remove)
       change manage_relationship(:user_profile, type: :direct_control)
     end
   end
