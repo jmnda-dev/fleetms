0c92161b3ef90ec23aca45dc999c7ab20072381e
Jachin Manda <manda.jachin482@gmail.com>
2024-05-01 01:37:01 +0200

    add application layout

--- lib/fleetms_web/components/layout_components.ex
+++ lib/fleetms_web/components/layout_components.ex
@@ -3,7 +3,7 @@ defmodule FleetmsWeb.LayoutComponents do
   This module contains components used in the layout of the application, for example `navbar`, `sidebar`, `public_navbar`, `footer`
   """
   use Phoenix.Component
-  import FleetmsWeb.CoreComponents, only: [button: 1]
+  import FleetmsWeb.CoreComponents, only: [button: 1, icon: 1]
   use FleetmsWeb, :verified_routes
 
   @doc """
@@ -66,6 +66,11 @@ defmodule FleetmsWeb.LayoutComponents do
             </div>
 
             <%= if @current_user do %>
+              <.link navigate={~p"/users"}>
+                <.button>
+                  Dashboard
+                </.button>
+              </.link>
               <button
                 class="flex items-center gap-4 hover:cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg p-2 mr-2.5"
                 id="user-menu-button"
@@ -210,5 +215,394 @@ defmodule FleetmsWeb.LayoutComponents do
       </nav>
     </header>
     """
+  end
+
+  def navbar(assigns) do
+    ~H"""
+    <nav
+      id="navbar"
+      phx-hook="initDarkModeToggle"
+      class="fixed z-30 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700"
+    >
+      <div class="py-3 px-3 lg:px-5 lg:pl-3">
+        <div class="flex justify-between items-center">
+          <div class="flex justify-start items-center">
+            <button
+              id="toggleSidebar"
+              aria-expanded="true"
+              aria-controls="sidebar"
+              class="hidden p-2 mr-3 text-gray-600 rounded cursor-pointer lg:inline hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700"
+            >
+              <svg
+                class="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                >
+                </path>
+              </svg>
+            </button>
+            <button
+              id="toggleSidebarMobile"
+              aria-expanded="true"
+              aria-controls="sidebar"
+              class="p-2 mr-2 text-gray-600 rounded cursor-pointer lg:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              <svg
+                id="toggleSidebarMobileHamburger"
+                class="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                >
+                </path>
+              </svg>
+              <svg
+                id="toggleSidebarMobileClose"
+                class="hidden w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                >
+                </path>
+              </svg>
+            </button>
+            <.link href={~p"/"} class="flex mr-14">
+              <img src="/images/logo_light.svg" class="dark:hidden mr-3 h-10" alt="Fleetms Logo" />
+              <img src="/images/logo_dark.svg" class="hidden dark:block mr-3 h-10" alt="Fleetms Logo" />
+            </.link>
+            <form action="#" method="GET" class="hidden lg:block lg:pl-2">
+              <label for="topbar-search" class="sr-only">Search</label>
+              <div class="relative mt-1 lg:w-96">
+                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                  <svg
+                    class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                      clip-rule="evenodd"
+                    >
+                    </path>
+                  </svg>
+                </div>
+                <input
+                  type="text"
+                  name="email"
+                  id="topbar-search"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Search"
+                />
+              </div>
+            </form>
+          </div>
+          <div class="flex items-center">
+            <button
+              id="toggleSidebarMobileSearch"
+              type="button"
+              class="p-2 text-gray-500 rounded-lg lg:hidden hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              <span class="sr-only">Search</span>
+
+              <svg
+                class="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clip-rule="evenodd"
+                >
+                </path>
+              </svg>
+            </button>
+
+            <button
+              type="button"
+              data-dropdown-toggle="notification-dropdown"
+              class="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700"
+            >
+              <span class="sr-only">View notifications</span>
+
+              <svg
+                class="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z">
+                </path>
+              </svg>
+            </button>
+
+            <div
+              class="hidden overflow-hidden z-20 z-50 my-4 max-w-sm text-base list-none bg-white rounded divide-y divide-gray-100 shadow-lg dark:divide-gray-600 dark:bg-gray-700"
+              id="notification-dropdown"
+            >
+              <div class="block py-2 px-4 text-base font-medium text-center text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                Notifications
+              </div>
+              <div>
+                <a
+                  href="#"
+                  class="flex py-3 px-4 border-b hover:bg-gray-100 dark:hover:bg-gray-600 dark:border-gray-600"
+                >
+                  <div class="flex-shrink-0">
+                    <img
+                      class="w-11 h-11 rounded-full"
+                      src="http://localhost:1313/images/users/bonnie-green.png"
+                      alt="Jese image"
+                    />
+                    <div class="flex absolute justify-center items-center ml-6 -mt-5 w-5 h-5 rounded-full border border-white bg-primary-700 dark:border-gray-700">
+                      <svg
+                        class="w-3 h-3 text-white"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z">
+                        </path>
+                        <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z">
+                        </path>
+                      </svg>
+                    </div>
+                  </div>
+                  <div class="pl-3 w-full">
+                    <div class="text-gray-500 font-normal text-sm mb-1.5 dark:text-gray-400">
+                      New message from <span class="font-semibold text-gray-900 dark:text-white">Bonnie Green</span>: "Hey, what's up? All set
+                      for the presentation?"
+                    </div>
+                    <div class="text-xs font-medium text-primary-700 dark:text-primary-400">
+                      a few moments ago
+                    </div>
+                  </div>
+                </a>
+              </div>
+              <a
+                href="#"
+                class="block py-2 text-base font-normal text-center text-gray-900 bg-gray-50 hover:bg-gray-100 dark:bg-gray-700 dark:text-white dark:hover:underline"
+              >
+                <div class="inline-flex items-center ">
+                  <svg
+                    class="mr-2 w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path>
+                    <path
+                      fill-rule="evenodd"
+                      d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                      clip-rule="evenodd"
+                    >
+                    </path>
+                  </svg>
+                  View all
+                </div>
+              </a>
+            </div>
+
+            <button
+              id="theme-toggle"
+              data-tooltip-target="tooltip-toggle"
+              type="button"
+              class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+            >
+              <svg
+                id="theme-toggle-dark-icon"
+                class="hidden w-5 h-5"
+                data-clone-attributes
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+              </svg>
+              <svg
+                id="theme-toggle-light-icon"
+                class="hidden w-5 h-5"
+                data-clone-attributes
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                >
+                </path>
+              </svg>
+            </button>
+            <div
+              id="tooltip-toggle"
+              role="tooltip"
+              class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip"
+            >
+              Toggle dark mode
+              <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
+
+            <div class="flex items-center ml-3">
+              <div>
+                <button
+                  type="button"
+                  class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                  id="user-menu-button-2"
+                  aria-expanded="false"
+                  data-dropdown-toggle="dropdown-2"
+                >
+                  <span class="sr-only">Open user menu</span>
+                  <img
+                    class="w-8 h-8 rounded-full"
+                    src="http://localhost:1313/images/users/neil-sims.png"
+                    alt="user photo"
+                  />
+                </button>
+              </div>
+
+              <div
+                class="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+                id="dropdown-2"
+              >
+                <div class="py-3 px-4" role="none">
+                  <p class="text-sm text-gray-900 dark:text-white" role="none">
+                    Neil Sims
+                  </p>
+                  <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                    neil.sims@flowbite.com
+                  </p>
+                </div>
+                <ul class="py-1" role="none">
+                  <li>
+                    <a
+                      href="#"
+                      class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                      role="menuitem"
+                    >
+                      Settings
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                      role="menuitem"
+                    >
+                      Sign out
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    """
+  end
+
+  def sidebar(assigns) do
+    ~H"""
+    <aside
+      id="sidebar"
+      phx-hook="initSidebarJs"
+      class="flex hidden fixed top-0 left-0 z-20 flex-col flex-shrink-0 pt-16 w-64 h-full duration-75 lg:flex transition-width"
+      aria-label="Sidebar"
+    >
+      <div class="flex relative flex-col flex-1 pt-0 min-h-0 bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <div class="flex overflow-y-auto flex-col flex-1 pt-5 pb-4">
+          <div class="flex-1 px-3 space-y-1 bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+            <ul class="pb-2 space-y-2">
+              <li>
+                <form action="#" method="GET" class="lg:hidden">
+                  <label for="mobile-search" class="sr-only">Search</label>
+                  <div class="relative">
+                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                      <svg
+                        class="w-5 h-5 text-gray-500"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                          clip-rule="evenodd"
+                        >
+                        </path>
+                      </svg>
+                    </div>
+                    <input
+                      type="text"
+                      name="email"
+                      id="mobile-search"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-200 dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      placeholder="Search"
+                    />
+                  </div>
+                </form>
+              </li>
+              <li :for={sidebar_item <- sidebar_links()}>
+                <.link
+                  navigate={sidebar_item.link}
+                  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700"
+                >
+                  <.icon name={sidebar_item.icon_name} class="ml-1 h-6 w-6" />
+
+                  <span class="ml-3" sidebar-toggle-item><%= sidebar_item.title %></span>
+                </.link>
+              </li>
+            </ul>
+            <div class="pt-2 space-y-2">
+              <a
+                href="https://flowbite.com/docs/components/alerts/"
+                target="_blank"
+                class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg transition duration-75 hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700"
+              >
+                <svg
+                  class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z">
+                  </path>
+                </svg>
+                <span class="ml-3" sidebar-toggle-item>Components</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </aside>
+    """
+  end
+
+  defp sidebar_links do
+    [
+      %{link: ~p"/dashboard", title: "Dashboard", icon_name: "hero-chart-pie"},
+      %{link: ~p"/users", title: "Users", icon_name: "hero-users"}
+    ]
   end
 end
