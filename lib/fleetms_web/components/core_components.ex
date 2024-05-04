da16be489bbc7dc3912fe9f144aad8364131dbcf
Jachin Manda <manda.jachin482@gmail.com>
2024-05-04 10:03:47 +0000

    update core components

--- lib/fleetms_web/components/core_components.ex
+++ lib/fleetms_web/components/core_components.ex
@@ -39,6 +39,10 @@ defmodule FleetmsWeb.CoreComponents do
   attr :id, :string, required: true
   attr :show, :boolean, default: false
   attr :on_cancel, JS, default: %JS{}
+  attr :max_width, :string, default: "max-w-3xl"
+  attr :phx_key, :string, default: "escape"
+  attr :phx_click_away, :boolean, default: true
+  attr :phx_window_keydown, :boolean, default: true
   slot :inner_block, required: true
 
   def modal(assigns) do
@@ -50,7 +54,11 @@ defmodule FleetmsWeb.CoreComponents do
       data-cancel={JS.exec(@on_cancel, "phx-remove")}
       class="relative z-50 hidden"
     >
-      <div id={"#{@id}-bg"} class="bg-zinc-50/90 fixed inset-0 transition-opacity" aria-hidden="true" />
+      <div
+        id={"#{@id}-bg"}
+        class="bg-gray-700/70 dark:bg-gray-900/70 fixed inset-0 transition-opacity"
+        aria-hidden="true"
+      />
       <div
         class="fixed inset-0 overflow-y-auto"
         aria-labelledby={"#{@id}-title"}
@@ -60,22 +68,22 @@ defmodule FleetmsWeb.CoreComponents do
         tabindex="0"
       >
         <div class="flex min-h-full items-center justify-center">
-          <div class="w-full max-w-3xl p-4 sm:p-6 lg:py-8">
+          <div class={["w-full p-4 sm:p-6 lg:py-8", @max_width]}>
             <.focus_wrap
               id={"#{@id}-container"}
-              phx-window-keydown={JS.exec("data-cancel", to: "##{@id}")}
-              phx-key="escape"
-              phx-click-away={JS.exec("data-cancel", to: "##{@id}")}
-              class="shadow-zinc-700/10 ring-zinc-700/10 relative hidden rounded-2xl bg-white p-14 shadow-lg ring-1 transition"
+              phx-window-keydown={@phx_window_keydown && JS.exec("data-cancel", to: "##{@id}")}
+              phx-key={@phx_key}
+              phx-click-away={@phx_click_away && JS.exec("data-cancel", to: "##{@id}")}
+              class="shadow-gray-700/10 ring-gray-700/10 relative hidden rounded-2xl bg-white p-14 shadow-lg ring-1 transition dark:bg-gray-800"
             >
               <div class="absolute top-6 right-5">
                 <button
                   phx-click={JS.exec("data-cancel", to: "##{@id}")}
                   type="button"
-                  class="-m-3 flex-none p-3 opacity-20 hover:opacity-40"
+                  class="-m-3 flex-none p-3"
                   aria-label={gettext("close")}
                 >
-                  <.icon name="hero-x-mark-solid" class="h-5 w-5" />
+                <.icon name="hero-x-mark-solid text-gray-800 dark:text-gray-100 font-bold" class="h-5 w-5" />
                 </button>
               </div>
               <div id={"#{@id}-content"}>
@@ -484,10 +492,10 @@ defmodule FleetmsWeb.CoreComponents do
     ~H"""
     <header class={[@actions != [] && "flex items-center justify-between gap-6", @class]}>
       <div>
-        <h1 class="text-lg font-semibold leading-8 text-zinc-800">
+        <h1 class="text-lg font-semibold leading-8 text-gray-800 dark:text-gray-100">
           <%= render_slot(@inner_block) %>
         </h1>
-        <p :if={@subtitle != []} class="mt-2 text-sm leading-6 text-zinc-600">
+        <p :if={@subtitle != []} class="mt-2 text-sm leading-6 text-gray-600 dark:text-gray-300">
           <%= render_slot(@subtitle) %>
         </p>
       </div>
