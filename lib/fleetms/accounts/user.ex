0e82bcd2c259a4c1afc5493193fb8c88845f9b9d
Jachin Manda <manda.jachin482@gmail.com>
2024-05-04 20:02:17 +0200

    add read and update actions

--- lib/fleetms/accounts/user.ex
+++ lib/fleetms/accounts/user.ex
@@ -75,7 +75,7 @@ defmodule Fleetms.Accounts.User do
   end
 
   actions do
-    defaults [:destroy, create: :*, update: :*]
+    defaults [:destroy, create: :*]
 
     create :register_with_password do
       allow_nil_input [:hashed_password]
@@ -139,7 +139,6 @@ defmodule Fleetms.Accounts.User do
       change set_context(%{strategy_name: :password})
       validate AshAuthentication.Strategy.Password.PasswordConfirmationValidation
       change AshAuthentication.Strategy.Password.HashPasswordChange
-      change AshAuthentication.GenerateTokenChange
 
       change manage_relationship(:organization_id, :organization, type: :append_and_remove)
       change manage_relationship(:user_profile, type: :direct_control)
@@ -149,6 +148,43 @@ defmodule Fleetms.Accounts.User do
       primary? true
 
       prepare build(load: [:full_name])
+    end
+
+    read :get_by_id do
+      argument :id, :uuid, allow_nil?: false
+
+      get? true
+      filter expr(id == ^arg(:id))
+      prepare build(load: [:full_name, :user_profile])
+    end
+
+    update :update do
+      primary? true
+      require_atomic? false
+      allow_nil_input [:hashed_password]
+      accept [:email, :roles]
+
+      argument :password, :string do
+        sensitive? true
+        constraints min_length: 8, max_length: 32
+      end
+
+      argument :password_confirmation, :string do
+        sensitive? true
+        constraints min_length: 8, max_length: 32
+      end
+
+      validate match(:email, ~r/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/) do
+        message "Invalid email format"
+      end
+
+      validate match(:password, ~r/^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/) do
+        message "Invalid password format. Password much contain at least one lowercase and uppercase letter, and at leat one of these characters: !#$%&?"
+      end
+
+      change set_context(%{strategy_name: :password})
+      validate AshAuthentication.Strategy.Password.PasswordConfirmationValidation
+      change AshAuthentication.Strategy.Password.HashPasswordChange
     end
   end
 
