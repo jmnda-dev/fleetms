ae43af29fd43670ef30127dace4fa1fef415d6d4
Jachin Manda <65728679+jmnda-dev@users.noreply.github.com>
2025-08-02 19:53:31 +0200

    enhancement: add flowbite component library

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,6 +24,7 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import {hooks as colocatedHooks} from "phoenix-colocated/fleetms"
 import topbar from "../vendor/topbar"
+import "flowbite/dist/flowbite.phoenix"
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 const liveSocket = new LiveSocket("/live", Socket, {
